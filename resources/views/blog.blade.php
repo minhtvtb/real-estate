8fad78a208d004ea66d64af78da66cceb06846fe
minhtv <tvminh2409@gmail.com>
2018-11-29 23:20:15 +0700

    fix

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -10,25 +10,26 @@
                     <div class="single-post">
                         <img class="img-fluid" src="img/blog/p1.jpg" alt="">
                         <ul class="tags">
-                            <li><a href="#">Art, </a></li>
+                            <li><a href="#">Chung cư cao cấp, </a></li>
                             <li><a href="#">Technology, </a></li>
-                            <li><a href="#">Fashion</a></li>
+                            <li><a href="#">Khu nghỉ dưỡng</a></li>
                         </ul>
-                        <a href="blog-single.html">
+                        <a href="#">
                             <h1>
-                                Cartridge Is Better Than Ever
-                                A Discount Toner
+                                Chúng tôi tin rằng
+                                bạn có nhà đẹp
+                                bạn thêm hạnh phúc!
                             </h1>
                         </a>
                         <p>
-                            MCSE boot camps have its supporters and its detractors. Some people do not understand why you should have to spend money on boot camp when you can get the MCSE study materials yourself at a fraction of the camp price. However, who has the willpower to actually sit through a self-imposed MCSE training. who has the willpower to actually sit through a self-imposed MCSE training.
+                            Bất cứ ai trên thế giới này đều mang trong mình ước mơ sở hữu một căn nhà đẹp, đầy đủ tiện nghi. Bạn thử tưởng tượng xem sau bao bộn bề cuộc sống nhà là chốn bình yên đón bạn trở về, vậy thì căn nhà đẹp sẽ mang đến cho bạn cảm giác hạnh phúc hơn bao giờ hết.
                         </p>
                         <div class="bottom-meta">
                             <div class="user-details row align-items-center">
                                 <div class="comment-wrap col-lg-6">
                                     <ul>
-                                        <li><a href="#"><span class="lnr lnr-heart"></span>	4 likes</a></li>
-                                        <li><a href="#"><span class="lnr lnr-bubble"></span> 06 Comments</a></li>
+                                        <li><a href="#"><span class="lnr lnr-heart"></span>	4 lượt thích</a></li>
+                                        <li><a href="#"><span class="lnr lnr-bubble"></span> 06 Bình luận</a></li>
                                     </ul>
                                 </div>
                                 <div class="social-wrap col-lg-6">
@@ -43,28 +44,31 @@
                             </div>
                         </div>
                     </div>
+                </div>
+                <div class="col-lg-8 post-list blog-post-list">
                     <div class="single-post">
                         <img class="img-fluid" src="img/blog/p2.jpg" alt="">
                         <ul class="tags">
-                            <li><a href="#">Art, </a></li>
+                            <li><a href="#">Chung cư cao cấp, </a></li>
                             <li><a href="#">Technology, </a></li>
-                            <li><a href="#">Fashion</a></li>
+                            <li><a href="#">Khu nghỉ dưỡng</a></li>
                         </ul>
-                        <a href="blog-single.html">
+                        <a href="#">
                             <h1>
-                                Cartridge Is Better Than Ever
-                                A Discount Toner
+                                Chúng tôi tin rằng
+                                bạn có nhà đẹp
+                                bạn thêm hạnh phúc!
                             </h1>
                         </a>
                         <p>
-                            MCSE boot camps have its supporters and its detractors. Some people do not understand why you should have to spend money on boot camp when you can get the MCSE study materials yourself at a fraction of the camp price. However, who has the willpower to actually sit through a self-imposed MCSE training. who has the willpower to actually sit through a self-imposed MCSE training.
+                            Bất cứ ai trên thế giới này đều mang trong mình ước mơ sở hữu một căn nhà đẹp, đầy đủ tiện nghi. Bạn thử tưởng tượng xem sau bao bộn bề cuộc sống nhà là chốn bình yên đón bạn trở về, vậy thì căn nhà đẹp sẽ mang đến cho bạn cảm giác hạnh phúc hơn bao giờ hết.
                         </p>
                         <div class="bottom-meta">
                             <div class="user-details row align-items-center">
                                 <div class="comment-wrap col-lg-6">
                                     <ul>
-                                        <li><a href="#"><span class="lnr lnr-heart"></span>	4 likes</a></li>
-                                        <li><a href="#"><span class="lnr lnr-bubble"></span> 06 Comments</a></li>
+                                        <li><a href="#"><span class="lnr lnr-heart"></span>	4 lượt thích</a></li>
+                                        <li><a href="#"><span class="lnr lnr-bubble"></span> 06 Bình luận</a></li>
                                     </ul>
                                 </div>
                                 <div class="social-wrap col-lg-6">
@@ -79,28 +83,31 @@
                             </div>
                         </div>
                     </div>
+                </div>
+                <div class="col-lg-8 post-list blog-post-list">
                     <div class="single-post">
                         <img class="img-fluid" src="img/blog/p3.jpg" alt="">
                         <ul class="tags">
-                            <li><a href="#">Art, </a></li>
+                            <li><a href="#">Chung cư cao cấp, </a></li>
                             <li><a href="#">Technology, </a></li>
-                            <li><a href="#">Fashion</a></li>
+                            <li><a href="#">Khu nghỉ dưỡng</a></li>
                         </ul>
-                        <a href="blog-single.html">
+                        <a href="#">
                             <h1>
-                                Cartridge Is Better Than Ever
-                                A Discount Toner
+                                Chúng tôi tin rằng
+                                bạn có nhà đẹp
+                                bạn thêm hạnh phúc!
                             </h1>
                         </a>
                         <p>
-                            MCSE boot camps have its supporters and its detractors. Some people do not understand why you should have to spend money on boot camp when you can get the MCSE study materials yourself at a fraction of the camp price. However, who has the willpower to actually sit through a self-imposed MCSE training. who has the willpower to actually sit through a self-imposed MCSE training.
+                            Bất cứ ai trên thế giới này đều mang trong mình ước mơ sở hữu một căn nhà đẹp, đầy đủ tiện nghi. Bạn thử tưởng tượng xem sau bao bộn bề cuộc sống nhà là chốn bình yên đón bạn trở về, vậy thì căn nhà đẹp sẽ mang đến cho bạn cảm giác hạnh phúc hơn bao giờ hết.
                         </p>
                         <div class="bottom-meta">
                             <div class="user-details row align-items-center">
                                 <div class="comment-wrap col-lg-6">
                                     <ul>
-                                        <li><a href="#"><span class="lnr lnr-heart"></span>	4 likes</a></li>
-                                        <li><a href="#"><span class="lnr lnr-bubble"></span> 06 Comments</a></li>
+                                        <li><a href="#"><span class="lnr lnr-heart"></span>	4 lượt thích</a></li>
+                                        <li><a href="#"><span class="lnr lnr-bubble"></span> 06 Bình luận</a></li>
                                     </ul>
                                 </div>
                                 <div class="social-wrap col-lg-6">
@@ -115,28 +122,31 @@
                             </div>
                         </div>
                     </div>
+                </div>
+                <div class="col-lg-8 post-list blog-post-list">
                     <div class="single-post">
                         <img class="img-fluid" src="img/blog/p4.jpg" alt="">
                         <ul class="tags">
-                            <li><a href="#">Art, </a></li>
+                            <li><a href="#">Chung cư cao cấp, </a></li>
                             <li><a href="#">Technology, </a></li>
-                            <li><a href="#">Fashion</a></li>
+                            <li><a href="#">Khu nghỉ dưỡng</a></li>
                         </ul>
-                        <a href="blog-single.html">
+                        <a href="#">
                             <h1>
-                                Cartridge Is Better Than Ever
-                                A Discount Toner
+                                Chúng tôi tin rằng
+                                bạn có nhà đẹp
+                                bạn thêm hạnh phúc!
                             </h1>
                         </a>
                         <p>
-                            MCSE boot camps have its supporters and its detractors. Some people do not understand why you should have to spend money on boot camp when you can get the MCSE study materials yourself at a fraction of the camp price. However, who has the willpower to actually sit through a self-imposed MCSE training. who has the willpower to actually sit through a self-imposed MCSE training.
+                            Bất cứ ai trên thế giới này đều mang trong mình ước mơ sở hữu một căn nhà đẹp, đầy đủ tiện nghi. Bạn thử tưởng tượng xem sau bao bộn bề cuộc sống nhà là chốn bình yên đón bạn trở về, vậy thì căn nhà đẹp sẽ mang đến cho bạn cảm giác hạnh phúc hơn bao giờ hết.
                         </p>
                         <div class="bottom-meta">
                             <div class="user-details row align-items-center">
                                 <div class="comment-wrap col-lg-6">
                                     <ul>
-                                        <li><a href="#"><span class="lnr lnr-heart"></span>	4 likes</a></li>
-                                        <li><a href="#"><span class="lnr lnr-bubble"></span> 06 Comments</a></li>
+                                        <li><a href="#"><span class="lnr lnr-heart"></span>	4 lượt thích</a></li>
+                                        <li><a href="#"><span class="lnr lnr-bubble"></span> 06 Bình luận</a></li>
                                     </ul>
                                 </div>
                                 <div class="social-wrap col-lg-6">
