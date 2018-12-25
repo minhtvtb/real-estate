4fc0e5d23980467564ba886047faff381323a35f
minhtv <tvminh2409@gmail.com>
2018-12-25 23:42:53 +0700

    debug

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,12 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * Controller constructor.
+     */
+    function __construct()
+    {
+        dd(\App\Entities\SetupInfo::all());
+    }
 }
