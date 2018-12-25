865effed0ecb7b156f64827011c658d7489d32fa
minhtv <tvminh2409@gmail.com>
2018-12-25 23:44:27 +0700

    debug

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -38,8 +38,8 @@
             <div class="col-6 top-head-right">
                 <ul>
                     <li>
-                        <a href="tel:{{getSetting('phone')->value}}" class="font-weight-bold" style="color: #fca940; font-size: 14px">
-                            Hotline : {{getSetting('phone')->value}}
+                        <a href="tel:" class="font-weight-bold" style="color: #fca940; font-size: 14px">
+                            Hotline :
                         </a>
                     </li>
                     <li><a href="#" style="font-size: 16px"><i class="fa fa-facebook social-header"></i></a></li>
@@ -94,7 +94,7 @@
                         <span style="color: #0e3950">Email:</span> ygmland@gmail.com
                     </p>
                     <p>
-                        Liên hệ: <a href="tel:{{getSetting('phone')->value}}">{{getSetting('phone')->value}}</a>
+                        Liên hệ: <a href="tel:"></a>
                     </p>
                     <p>
                         website: <a href="http://ygm.com.vn">ygm.com.vn</a>
